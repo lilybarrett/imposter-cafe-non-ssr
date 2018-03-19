5f64be16ef8f60d45dda06b11afe980764ec5d9a
lilybarrett <lily@barrett.us>
2018-03-19 18:55:16 -0400

    Set up react router

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,34 +1,14 @@
-import React, { Component } from "react";
-import { Container, Header } from "./styles";
+import React from "react";
+import NavBar from "../nav-bar";
+import Main from "../main";
 
-class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      menuItems: []
-    }
-  }
-  
-  async componentDidMount() {
-    const res = await fetch("/menu");
-    const menu = await res.json();
-    this.setState({ menuItems: menu.menuItems });
-  }
-  render() {
+const App = () => {
     return (
-      <Container>
-          <Header>Imposter Cafe Brunch Menu</Header>
-          { this.state.menuItems.map((menuItem) => {
-            const { name, description, cost } = menuItem;
-            return (
-              <div>
-                {name}: {description} - Price: {cost}
-              </div>
-            )
-          }) }
-      </Container>
+        <div>
+            <NavBar />
+            <Main />
+        </div> 
     );
-  }
 }
 
 export default App;
